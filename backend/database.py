d27397121c7fbc4e70f596517604d90f82f37214
misiol7 <147823335+misiol7@users.noreply.github.com>
2026-02-15 18:04:48 +0100

    Delete backend directory

--- backend/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://shelter:shelter@postgres:5432/shelter"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
